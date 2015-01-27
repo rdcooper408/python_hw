bcf1e43e462d23ca3114dbb39d0174d94f62af8a
Robert Cooper <rdcooper408@gmail.com>
2015-01-27 06:54:36 +0000

    Add Ex2

---
+++ Ex2.py
@@ -0,0 +1,7 @@
+#Ex.2)
+C_DNA = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+C_cDNA = C_DNA.replace('A', 't')
+C_cDNA = C_cDNA.replace('C', 'g')
+C_cDNA = C_cDNA.replace('G', 'c')
+C_cDNA = C_cDNA.replace('T', 'a')
+print(C_cDNA)
